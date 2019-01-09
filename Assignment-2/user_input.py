db04d37d93f2ccc4fdd343d0504398769f555f5a
Anusha Neupane <AnushaNeupane@users.github.com>
2019-01-08 21:27:27 -0600

    CS-I Assignment 2

---
+++ Assignment-2/user_input.py
@@ -0,0 +1,27 @@
+# Asks the user for 5 words, each of a specific part of speech.
+a= input("Enter exclamatory word:") 
+b=input("Enter adverb:")
+c=input("Enter noun:")
+d=input("Enter adjective:")
+input()
+
+# Prints out a Mad Lib story using those words.
+print("'"+ a+"! beautiful"+"'," "he said "+ b +" as she approached near him and took "+ c +" from his hand and went away with her "+ d +" friend.")
+
+
+# Asks the user for a whole number.
+a = int(input())
+
+# Prints number plus number.
+print(a, '+', a,'is',a+a)
+
+
+# Prints number minus 2.
+print(a,'-',2,'is',a-2)
+
+# Prints number times number.
+print(a,'*',a,'is',a*a)
+
+
+# Prints number divided by 2.
+print(a,'/',2,'is',a/2)
