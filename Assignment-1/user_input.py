e56cfbc39ce6f8ac49f05b5858d092eafb8f32a2
Anusha Neupane <AnushaNeupane@users.github.com>
2019-01-08 21:19:01 -0600

    First assignment

---
+++ Assignment-1/user_input.py
@@ -0,0 +1,41 @@
+# Asks the user for two numbers.
+first_numb= input()
+second_numb= input()
+
+# Prints the product of the numbers.
+product= int(first_numb)*int(second_numb)
+print(product)
+
+# Asks the user for a word.
+a=input()
+a=str(a)
+
+# Asks the user for a whole number.
+b=input()
+b=int(b)
+
+# Prints the word the given number of times.
+print(a*b)
+
+# Asks the user for a fruit.
+a= input()
+a= str(a)
+
+# Prints the first fruit and the number 1.
+print(a + "1")
+
+# Asks the user for a fruit.
+b=input()
+b=str(b)
+
+# Prints the previous, the second fruit, and the number 2.
+print(a + "1"+b +"2")
+
+# Asks the user for a fruit.
+c=input()
+c= str(c)
+
+# Prints the previous, the third fruit, and the number 3.
+print(a +"1"+b +"2"+c+ "3")
+
+# TODO
